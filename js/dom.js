4c33d5b724895f1a6efe87864a0aa54e29e36a68
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-02-28 12:10:41 -0500

    61. DOM Introdución

---
+++ js/dom.js
@@ -0,0 +1,34 @@
+/*console.log(window);
+console.log(document);
+        
+let texto = "Hola, soy tu amigo y docente digital. Jonathan MirCha";
+const hablar = (texto) => speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+        
+hablar(texto);*/
+
+/*
+
+
+*/
+console.log("********** Elementos del Documento **********");
+console.log(window.document);
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.doctype);
+console.log(document.characterSet);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
+
+setTimeout(() => {
+    console.log(document.getSelection().toString())
+}, 3000);
+
+document.write("<h2>Hola Mundo desde el DOM</h2>");
+
+
