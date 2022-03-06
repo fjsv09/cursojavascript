67115d481a76c8ab3aba32b2bef55caaa7f30630
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-03-06 14:59:46 -0500

    64. DOM: Estilos y Variables CSS

--- js/dom.js
+++ js/dom.js
@@ -64,7 +64,7 @@ console.log(document.querySelectorAll("#menu li"));
 
 
 */
-console.log(document.documentElement.lang);
+/*console.log(document.documentElement.lang);
 console.log(document.documentElement.getAttribute("lang"));
 console.log(document.querySelector(".link-dom").href);
 console.log(document.querySelector(".link-dom").getAttribute("href"));
@@ -91,4 +91,52 @@ console.log(linkDOM.dataset.description);
 linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento");
 console.log(linkDOM.dataset.description);
 linkDOM.dataset.description = "Suscríbete a mi canal y comparte";
-console.log(linkDOM.dataset.description);
+console.log(linkDOM.dataset.description);*/
+/*
+
+
+
+
+
+*/
+
+const linkDOM = document.querySelector(".link-dom");
+
+console.log(linkDOM.style);
+console.log(linkDOM.getAttribute("style"));
+console.log(linkDOM.style.backgroundColor);
+console.log(linkDOM.style.color);
+console.log(window.getComputedStyle(linkDOM));
+console.log(window.getComputedStyle(linkDOM).getPropertyValue("color"));
+
+
+linkDOM.style.setProperty("text-decoration", "none");
+linkDOM.style.setProperty("display", "block");
+linkDOM.style.width = "50%"
+linkDOM.style.textAlign = "center";
+linkDOM.style.marginLeft = "auto"
+linkDOM.style.marginRight = "auto";
+linkDOM.style.padding = "1rem";
+linkDOM.style.borderRadius = ".5rem";
+
+console.log(linkDOM.style);
+console.log(linkDOM.getAttribute("style"));
+console.log(window.getComputedStyle(linkDOM));
+
+//Variable CSS - Custom Properties CSS
+const html = document.documentElement,
+body = document.body;
+
+let varDarkColor = getComputedStyle(html).getPropertyValue("--dark-color");
+let varYellowColor = getComputedStyle(html).getPropertyValue("--yellow-color");
+console.log(varDarkColor);
+console.log(varYellowColor);
+
+body.style.backgroundColor = varDarkColor;
+body.style.color = varYellowColor;
+
+html.style.setProperty("--dark-color", "#000");
+varDarkColor = getComputedStyle(html).getPropertyValue("--dark-color");
+
+body.style.setProperty("background-color", varDarkColor);
+
