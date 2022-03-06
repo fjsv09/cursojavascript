80761558030c26b113074c6839e653f2aa889f72
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-03-06 15:48:00 -0500

    65. DOM: Clases CSS

--- js/dom.js
+++ js/dom.js
@@ -100,7 +100,7 @@ console.log(linkDOM.dataset.description);*/
 
 */
 
-const linkDOM = document.querySelector(".link-dom");
+/*const linkDOM = document.querySelector(".link-dom");
 
 console.log(linkDOM.style);
 console.log(linkDOM.getAttribute("style"));
@@ -139,4 +139,37 @@ html.style.setProperty("--dark-color", "#000");
 varDarkColor = getComputedStyle(html).getPropertyValue("--dark-color");
 
 body.style.setProperty("background-color", varDarkColor);
+*/
+
+/*
+
+
+
+
+
+
+*/
+const card = document.querySelector(".card");
+console.log(card);
+console.log(card.className);
+console.log(card.classList);
+console.log(card.classList.contains("rotate-45"));
+card.classList.add("rotate-45");
+console.log(card.classList.contains("rotate-45"));
+console.log(card.className);
+console.log(card.classList);
+card.classList.remove("rotate-45");
+console.log(card.classList.contains("rotate-45"));
+card.classList.toggle("rotate-45");
+console.log(card.classList.contains("rotate-45"));
+card.classList.toggle("rotate-45");
+console.log(card.classList.contains("rotate-45"));
+card.classList.toggle("rotate-45");
+card.classList.replace("rotate-45", "rotate-135");
+card.classList.add("opacity-80", "sepia");
+card.classList.remove("opacity-80", "sepia");
+card.classList.toggle("opacity-80");
+card.classList.toggle("sepia");
+
+
 
