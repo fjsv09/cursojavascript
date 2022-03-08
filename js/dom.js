4d175e48c48635e73c45bde7bc98a96fbbf987de
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-03-08 12:50:49 -0500

    68. DOM: Creando Elementos y Fragmentos

--- js/dom.js
+++ js/dom.js
@@ -210,7 +210,7 @@ whatIsDOM.innerHTML = text;
 
 
 */
-const cards = document.querySelector(".cards");
+/*const cards = document.querySelector(".cards");
 
 console.log(cards);
 console.log(cards.children);
@@ -223,9 +223,84 @@ console.log(cards.previousElementSibling);
 console.log(cards.nextElementSibling);
 console.log(cards.closest("div"));
 console.log(cards.closest("body"));
-console.log(cards.children[3].closest("section"));
+console.log(cards.children[3].closest("section"));*/
+/*
 
 
 
 
 
+*/
+const figure = document.createElement("figure"),
+img = document.createElement("img"),
+figcaption = document.createElement("figcaption"),
+figcaptionText = document.createTextNode("Animals"),
+cards = document.querySelector(".cards");
+const figure2 = document.createElement("figure");
+
+
+img.setAttribute("src", "https://placeimg.com/200/200/animals");
+img.setAttribute("alt", "Animals");
+figure.classList.add("card")
+
+figcaption.appendChild(figcaptionText);
+figure.appendChild(img);
+figure.appendChild(figcaption);
+cards.appendChild(figure);
+
+
+figure2.innerHTML= `
+    <img src="https://placeimg.com/200/200/people" alt="People">
+    <figcaption>People</figcaption>
+`;
+
+figure2.classList.add("card");
+cards.appendChild(figure2);
+
+const estaciones = ["Primavera", "Verano", "Otoño", "Invierno"],
+ul = document.createElement("ul")
+
+document.write("<h3>Estaciones del Año</h3>");
+document.body.appendChild(ul);
+
+estaciones.forEach(el => {
+    const li = document.createElement("li");
+    li.textContent = el;
+    ul.appendChild(li);
+});
+
+const continentes = ["Africa", "América", "Asia", "Europa", "Oceanía"],
+ul2 = document.createElement("ul");
+
+document.write("<h3>Continentes del mundo</h3>");
+document.body.appendChild(ul2);
+
+ul2.innerHTML = "";
+continentes.forEach(el => (ul2.innerHTML += `<li>${el}</li`));
+
+const meses = ["Enero",
+"Febrero",
+"Marzo",
+"Abril",
+"Mayo",
+"Junio",
+"Julio",
+"Agosto",
+"Septiembre",
+"Octubre",
+"Noviembre",
+"Diciembre"
+];
+
+ul3 = document.createElement("lu");
+fragmento = document.createDocumentFragment();
+
+meses.forEach(el => {
+    const li = document.createElement("li");
+    li.textContent = el;
+    fragmento.appendChild(li);
+});
+
+document.write("<h3>Meses del Año</h3>");
+ul3.appendChild(fragmento);
+document.body.appendChild(ul3);
