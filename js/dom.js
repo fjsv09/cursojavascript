90d6276dce0eb8910480dad1bb13b87b02c716c4
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-03-07 23:19:39 -0500

    66. DOM: Texto y HTML

--- js/dom.js
+++ js/dom.js
@@ -149,6 +149,7 @@ body.style.setProperty("background-color", varDarkColor);
 
 
 */
+/*
 const card = document.querySelector(".card");
 console.log(card);
 console.log(card.className);
@@ -170,6 +171,33 @@ card.classList.add("opacity-80", "sepia");
 card.classList.remove("opacity-80", "sepia");
 card.classList.toggle("opacity-80");
 card.classList.toggle("sepia");
+*/
+/*
 
 
+
+
+
+
+*/
+
+const whatIsDOM = document.getElementById("que-es");
+
+let text = `
+    <p>
+        El Modelo de Objetos del Documento (<b> <i>DOM - Document Object Model</i></b>) es un API para documentos HTML y XML.
+    </p>
+    <p>
+        Éste proveé una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
+    </p>
+    <p>
+        <mark>El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.
+        </mark>
+    </p>|
+`;
+
+// whatIsDOM.innerText = text;
+whatIsDOM.textContent = text;
+whatIsDOM.innerHTML = text;
+// whatIsDOM.outerHTML = text;
 
