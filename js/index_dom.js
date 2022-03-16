2ca956fb4f20bd6132c356831bcd5f3d1acf1b6e
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-03-16 18:10:19 -0500

    83. DOM: Ejercicios Prácticos | Alarma Sonora

--- js/index_dom.js
+++ js/index_dom.js
@@ -6,4 +6,5 @@ const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
+    alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
 })
