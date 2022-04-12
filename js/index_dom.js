b8fa8af290a3cb4f6737bcdeda0b23d6de5001b2
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-04-12 16:42:04 -0500

    100. DOM: Video Inteligente (IntersectionObserver & VisibilityChange)

--- js/index_dom.js
+++ js/index_dom.js
@@ -14,6 +14,7 @@ import scrollSpy from "./dom/scroll_espia.js";
 import draw from "./dom/sorteo.js";
 import { moveBall, shortcts } from "./dom/teclado.js";
 import darkTheme from "./dom/tema_oscuro.js";
+import smartVideo from "./dom/video_inteligente.js";
 
 
 const d = document;
@@ -48,6 +49,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     draw("#winner-btn", ".player");
     slider();
     scrollSpy();
+    smartVideo();
 });
 
 d.addEventListener("keydown", (e) => {
