b687993de6e3e11dd03956c55798e5d276254d91
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-04-13 21:34:35 -0500

    102. DOM: Ejercicios Prácticos | Validación de Formularios

--- js/index_dom.js
+++ js/index_dom.js
@@ -14,6 +14,7 @@ import scrollSpy from "./dom/scroll_espia.js";
 import draw from "./dom/sorteo.js";
 import { moveBall, shortcts } from "./dom/teclado.js";
 import darkTheme from "./dom/tema_oscuro.js";
+import contactFormValidations from "./dom/validaciones_formulario.js";
 import smartVideo from "./dom/video_inteligente.js";
 
 
@@ -49,7 +50,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
     draw("#winner-btn", ".player");
     slider();
     scrollSpy();
-    smartVideo();
+    smartVideo();contactFormValidations();
+
 });
 
 d.addEventListener("keydown", (e) => {
