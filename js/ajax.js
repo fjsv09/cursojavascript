2f1c02fe108528b131f1d370c93cf8f0fadee640
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-04-19 14:39:10 -0500

    106. AJAX: Objeto XMLHttpRequest

---
+++ js/ajax.js
@@ -0,0 +1,49 @@
+(() => {
+    // USO DE XMLHttpRequest
+    // PRIMER PASO: Crear una instancia de XMLHttpRequest()
+    const xhr = new XMLHttpRequest(),
+    $xhr = document.getElementById("xhr"),
+    $fragment = document.createDocumentFragment();
+    
+    
+    // SEGUNDO PASO: Asignar el o los eventos que se van usar en la petición
+    xhr.addEventListener("readystatechange", (e) => {
+        
+        if (xhr.readyState !== 4) return;
+        
+        console.log(xhr);        
+        
+        if (xhr.status >= 200 && xhr.status < 300) {
+            console.log("ÉXITO");        
+            // console.log(xhr.responseText);        
+            let json = JSON.parse(xhr.responseText)
+            console.log(json);
+
+            json.forEach(el => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`
+                $fragment.appendChild($li);
+            });
+
+            $xhr.appendChild($fragment);
+
+        } else {
+            console.log("ERROR");      
+            let message = xhr.statusText || "Ocurrió un error"
+            $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+        }
+
+        console.log("Este mensaje cargará de cualquier forma");
+        
+    });
+    
+    // TERCER PASO: Abrir la petición, estblecer el metodo (GET) y por ultimo el recurso o URL (https://jsonplaceholder.typicode.com/users)
+    // API publica
+    // xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+    // Archivo json local
+    xhr.open("GET", "assets/users.json");
+
+    // CUARTO PASO: Enviar la petición
+    xhr.send();
+    
+})();
