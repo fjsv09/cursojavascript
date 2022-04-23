b6dc251f103e2e3082b2a9870543aefae4d8e349
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-04-23 15:29:48 -0500

    110. AJAX: Librería Axios + Async-Await

--- js/ajax.js
+++ js/ajax.js
@@ -146,6 +146,37 @@
         $axios.innerHTML = `Error ${err.response.status}: ${message}`;
     })
     .finally(() => {
-        console.log("Esto se ejecturá independientemente del resultado Axios");
+        // console.log("Esto se ejecturá independientemente del resultado Axios");
     });
+})();
+
+
+
+// QUINTA FORMA DE TRABAJAR CON JSON AXIOS - ASYNC AWAIT
+(() => {
+    const $axiosAsync = document.getElementById("axios-async"),
+    $fragment = document.createDocumentFragment();
+
+    async function getData() {
+        try {
+            let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+            json = await res.data;
+
+            json.forEach(el => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`
+                $fragment.appendChild($li);
+            });
+                
+            $axiosAsync.appendChild($fragment)
+
+        } catch (err) {
+            let message = err.response.statusText || "Ocurrió un error"
+            $axiosAsync.innerHTML = `Error ${err.response.status}: ${message}`;
+        } finally {
+            console.log("Esto se ejecturá independientemente del resultado Axios");
+        }
+    }
+
+    getData();
 })();
