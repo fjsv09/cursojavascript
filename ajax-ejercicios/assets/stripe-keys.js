8cf7d3323181678e225542971cfd0075edb09199
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2022-07-24 12:00:29 -0500

    132. Ejercicios AJAX - APIs: Pagos Online con Fetch y Stripe (4/4)

---
+++ ajax-ejercicios/assets/stripe-keys.js
@@ -0,0 +1,6 @@
+export default {
+  public:
+    "pk_test_51LNMj0CFrlzZTHU832tMQzmQc75DSCMe8rj79eMwYAYWfBWekqvDyntFkUYBdQvNuntZwyxA1Ybk0kkPFti7bCKo003isTuoQR",
+  secret:
+    "sk_test_51LNMj0CFrlzZTHU8z88a7LLtxwKubBHt3d2uedqTsCPDcyR2PXWUZD9btAXbV9VzGDB2iUSAGviBsO26FuEGovcE005lNl2Qeg",
+};
